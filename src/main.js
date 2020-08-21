4c5a46a0b9833da970ff4f6c331498b293171eb8
DenilsonDonolato <42578438+DenilsonDonolato@users.noreply.github.com>
2020-08-21 08:31:11 -0300

    Commit

--- src/main.js
+++ src/main.js
@@ -2,9 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import BaseIcon from '@/components/BaseIcon.vue'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
 
-Vue.components('BaseIcon', BaseIcon)
+const requireComponent = require.context(
+  './components',
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+)
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+  )
+
+  console.log('file:' + fileName)
+  console.log('name:' + componentName)
+  Vue.component(componentName, componentConfig.defaul || componentConfig)
+})
 
 Vue.config.productionTip = false
 
